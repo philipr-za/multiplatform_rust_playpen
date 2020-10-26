dff715a44a655c694f944956e15488860c3983bb
Philip Robinson <simian@tari.com>
2020-10-26 11:22:58 +0200

    OS level exclusive file lock test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use chrono::Utc;
+use std::{io, thread};
+use std::time::Duration;
+use fs2::FileExt;
+use std::io::Error;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting");
+
+    let path = "/tmp/testfile.tst";
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open(path)
+        .unwrap();
+    //
+    // for _ in 0..5 {
+    //     let time = Utc::now();
+    //     if let Err(e) = writeln!(file, "{}", time) {
+    //         eprintln!("Couldn't write to file: {}", e);
+    //     }
+    //     thread::sleep(Duration::from_secs(1));
+    // }
+
+    println!("Trying to get lock - {}", Utc::now());
+
+
+    for _ in 0..10 {
+        match file.try_lock_exclusive() {
+            Ok(_) => {
+                println!("Got lock @ {}", Utc::now() );
+                break;
+            },
+            Err(e) => {
+                println!("failed to get lock @ {} - {:?}", Utc::now(), e);
+                thread::sleep(Duration::from_secs(1));
+            },
+        }
+
+    }
+
+    println!("Finished lock - {}", Utc::now());
+
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(n) => {
+            println!("{} bytes read", n);
+            println!("{}", input);
+        }
+        Err(error) => println!("error: {}", error),
+    }
+    println!("Finished");
 }
